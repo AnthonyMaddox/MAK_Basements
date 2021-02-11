a50e8ccc5cc92f468860e6f86b898c1ae95c811c
AnthonyMaddox <anthonyjmaddox@gmail.com>
2021-02-11 11:44:47 -0500

    installed react burger menu

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Kitchenette from "./Components/Kitchenette";
 import Bathroom from "./Components/Bathroom";
 import "./App.css";
 import { Route, Link, Redirect } from "react-router-dom";
-import logo from "./Images/MAK_Basement_Logo.jpg"
+import logo from "./Images/MAK_Basement_Logo.jpg";
+import { slide as Menu } from "react-burger-menu";
 
 //route is telling the app what component to render when something is clicked on (defining path)
 //Link replaces a tag from traditional html (what to click on to go somewhere)
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
@@ -20,22 +20,32 @@ class App extends Component {
       <div className="app">
         <nav className="navBar">
           <Link className="navLink" to="/home">
-            <div className="logoDiv"><img className="logoImg" src={logo} alt="MAK Basements Home" /></div>
-          </Link>
-            <div className="sideNav">
-                <Link className="navLink" to="/kitchenettes">
-                    <p className="linkText">KITCHENETTES</p>
-                </Link>
-                <Link className="navLink" to="/bathrooms">
-                    <p className="linkText">BATHROOMS</p>
-                </Link>
+            <div className="logoDiv">
+              <img className="logoImg" src={logo} alt="MAK Basements Home" />
             </div>
+          </Link>
+          <div className="sideNav">
+            <Link className="navLink" to="/kitchenettes">
+              <p className="linkText">KITCHENETTES</p>
+            </Link>
+            <Link className="navLink" to="/bathrooms">
+              <p className="linkText">BATHROOMS</p>
+            </Link>
+            <Menu>
+              <Link className="navLink" to="/kitchenettes">
+                <p className="linkText">KITCHENETTES</p>
+              </Link>
+              <Link className="navLink" to="/bathrooms">
+                <p className="linkText">BATHROOMS</p>
+              </Link>
+            </Menu>
+          </div>
         </nav>
         <main>
-        <Route path="/" render={() => <Redirect to="/home" />} />
-        <Route path="/home" component={Home} exact={true} />
-        <Route path="/kitchenettes" component={Kitchenette} />
-        <Route path="/bathrooms" component={Bathroom} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route path="/home" component={Home} exact={true} />
+          <Route path="/kitchenettes" component={Kitchenette} />
+          <Route path="/bathrooms" component={Bathroom} />
         </main>
       </div>
     );

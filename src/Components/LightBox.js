e75a003fda69c6fd31353849ca294ef7b02575a2
AnthonyMaddox <anthonyjmaddox@gmail.com>
2021-02-19 14:21:42 -0500

    installed lightbox pro

--- src/Components/LightBox.js
+++ src/Components/LightBox.js
@@ -1,21 +1,48 @@
 import React, { useState } from "react";
 import FsLightbox from "fslightbox-react";
 import blue_shark from "../Images/blue_shark.JPG";
+import bg from "../Images/basement_bg.jpg";
 import "../App.css";
 
 function LightBox() {
   // if toggler is updated when lightbox is closed it will open it
   // if toggler is updated when lightbox is opened it will close it
-  const [toggler, setToggler] = useState(false);
+  //   const [toggler, setToggler] = useState(false);
+  const [lightboxController, setLightboxController] = useState({
+    toggler: false,
+    slide: 1,
+  });
+
+  function openLightboxOnSlide(number) {
+    setLightboxController({
+      toggler: !lightboxController.toggler,
+      slide: number,
+    });
+  }
 
   return (
     <>
-      <button onClick={() => setToggler(!toggler)}>Toggle Lightbox</button>
+      <div>
+        <img
+          className="lbPic"
+          src={blue_shark}
+          onClick={() => openLightboxOnSlide(1)}
+        />
+      </div>
+      <div>
+        <img
+          className="lbPic"
+          src={bg}
+          onClick={() => openLightboxOnSlide(2)}
+        />
+      </div>
       <FsLightbox
-        toggler={toggler}
-        sources={[blue_shark, blue_shark]}
+        toggler={lightboxController.toggler}
+        sources={[blue_shark, bg]}
+        thumbs={[blue_shark, bg]}
         initialAnimation="scale-in-long"
         slideChangeAnimation="scale-in"
+        slide={lightboxController.slide}
       />
     </>
   );

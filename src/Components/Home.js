6752e358c1bbe7ed68c0d7daec77fe1eb4002536
AnthonyMaddox <anthonyjmaddox@gmail.com>
2021-03-11 12:28:00 -0500

    wobble delay

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -34,11 +34,7 @@ class Home extends Component {
           </ScrollAnimation>
 
           <a className="phoneLink" href="tel:240-766-4873">
-            <ScrollAnimation
-              offset="75"
-              initiallyVisible={true}
-              animateIn="wobble"
-            >
+            <ScrollAnimation offset="75" delay="1000" animateIn="wobble">
               <FontAwesomeIcon className="phoneIcon" icon="phone" />
             </ScrollAnimation>
             <ScrollAnimation offset="75" animateIn="bounceInRight">
